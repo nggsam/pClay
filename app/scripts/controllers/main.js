18f01207f9ee15dc04a5755f77f3c7b344574e1a
exfoxz <chanhnp@gmail.com>
2015-01-31 19:28:53 -0500

    add operation pdb and surfgen to interface

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,113 @@
  * Controller of the pclayApp
  */
 angular.module('pclayApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+   	$scope.floatingClass = 'floating-blur';
+    $scope.mouseFloating = function () {
+        if(this.mouseover) {
+            console.log(this);
+            $scope.floatingClass = 'floating-blur';
+            this.mouseover = false;
+        } else {
+            console.log(this);
+            $scope.floatingClass = 'floating-show';
+            this.mouseover = true;
+        }
+    };
+
+    // Init scene
+    $scope.glmol = new GLmol('glmolX', true, 'canvas');
+    $scope.glmol.init();
+
+	// Url for VASPI Server
+    var SERVER = 'http://localhost:8000/';
+
+	function init(scope) {
+  		//dummy object for input
+		scope.input = {};
+	  	scope.show = {
+	       dropzone: true,
+	       surfaces: false,
+	       structures: false
+	   	};
+	   	//list of pdbs
+	   	scope.pdbList = [];
+	  	//list of surf meshes
+  	 	scope.surfList = [];
+  	 	//progress bar
+  		scope.progress = {};
+
+  		scope.ops = {
+  			'surfgen': 'pdb?op=surfgen&id=',
+  			'pdb': 'pdb?op=pdb&id='
+  		}
+	};
+
+   	init($scope);
+
+   	$scope.handleError = function(err){
+   		console.log("Error:", err)
+   	}
+   	 /** reset $scope.input */
+    $scope.input.reset = function () {
+        $scope.input.name = '';
+    };
+
+	/** Get PDB info from server and add it to scene */
+    $scope.fetchPdb = function (id) {
+//            check for undefined or empty input
+    	if ($scope.input.name == undefined || $scope.input.name == '') {
+            alert('Please enter PDB ID.');
+        } 
+        else {
+	        // Reset input
+	        $scope.input.reset();
+	        $http.get(SERVER + $scope.ops['pdb'] + id)
+		        .success(function(data){
+		    		console.log('Fetching', id, 'done.')
+		        	$scope.glmol.addPDB(id, data);
+		        })
+				.error(function(err){
+					$scope.handleError(err);
+				});
+        }	
+    };
+
+ 	/** Get pdb with id and add to scene  */
+    $scope.fetchSurf = function (id, color) {
+    	if ($scope.input.name == undefined || $scope.input.name == '') {
+            alert('Please enter Surface ID.');
+        } 
+        else {
+	        // Reset input
+	        $scope.input.reset();
+	        $http.get(SERVER + $scope.ops['surfgen'] + id)
+		        .success(function(data){
+		    		console.log('Fetching', id, 'done.')
+		        	$http.get(data.url)
+		        		.success(function(data){
+	        				/* Parse data and render to canvas */
+                			console.log("fetchPdb success");
+                			console.log(data);
+                			$scope.glmol.addSurf(data, color);
+		        		})
+		        		.error(function(err){
+		        			$scope.handleError(err);
+		        		})
+		        		// .then(function(progress){
+		        		// 	console.log("fetchPdbAsync::Progress()");
+		        		// 	console.log(progress);
+		        		// })
+		        })
+				.error(function(err){
+					$scope.handleError(err);
+				});
+        }	
+	};			
+});
