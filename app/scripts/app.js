f8e7662c1e93310a1d4623ad117ae714ba764ada
exfoxz <chanhnp@gmail.com>
2015-03-16 14:57:31 -0400

    change SERVER URL

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,7 +23,7 @@ angular
   ])
     .constant('config', {
         appName: 'pClay',
-        server: 'http://54.65.95.7:8000/'
+        server: 'http://54.165.42.10:8000/' //TODO: make an ENV for this
     })
     .config(function ($routeProvider) {
         $routeProvider
